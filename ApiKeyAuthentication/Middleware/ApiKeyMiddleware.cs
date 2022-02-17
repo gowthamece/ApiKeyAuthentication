14c4f44daf606514928c7ce656cb0a2f214e528f
Gowtham K <gowthamk91@outlook.com>
2022-02-17 21:16:03 +0530

    Add project files.

---
+++ ApiKeyAuthentication/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiKeyAuthentication.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEY = "XApiKey";
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided ");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
